4c64d71d0747be525542ccf5eae62e1fb58e2fd4
Hitesh1799 <hiteshdubey400@gmail.com>
2026-05-24 17:46:11 +0530

    render backend url changes

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-backend-hvgt.onrender.com",
     withCredentials: true,
 })
 
